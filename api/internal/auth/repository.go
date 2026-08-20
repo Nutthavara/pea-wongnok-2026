1e444e72d47be4f04d32c9d71d0cd80c45010581
chonlawit <chonlawit@odds.team>
2026-08-20 13:16:43 +0700

    refactor: redis get + del to amotic function with getdel

--- api/internal/auth/repository.go
+++ api/internal/auth/repository.go
@@ -30,16 +30,15 @@ func (repo *repository) SaveState(ctx context.Context, state string, ttl time.Du
 func (repo *repository) ConsumeState(ctx context.Context, state string) error {
 	key := oauthPrefix + state
 
-	exists, err := repo.cache.Exists(ctx, key).Result()
-	if err != nil {
+	if _, err := repo.cache.GetDel(ctx, key).Result(); err != nil {
+		if errors.Is(err, redis.Nil) {
+			return ErrNotFound
+		}
+
 		return err
 	}
 
-	if exists == 0 {
-		return ErrNotFound
-	}
-
-	return repo.cache.Del(ctx, key).Err()
+	return nil
 }
 
 func (repo *repository) SaveTicket(ctx context.Context, ticket string, credential Credential, ttl time.Duration) error {
@@ -54,7 +53,7 @@ func (repo *repository) SaveTicket(ctx context.Context, ticket string, credentia
 func (repo *repository) ConsumeTicket(ctx context.Context, ticket string) (Credential, error) {
 	key := oauthPrefix + ticket
 
-	payload, err := repo.cache.Get(ctx, key).Bytes()
+	payload, err := repo.cache.GetDel(ctx, key).Bytes()
 	if err != nil {
 		if errors.Is(err, redis.Nil) {
 			return Credential{}, ErrNotFound
@@ -62,8 +61,6 @@ func (repo *repository) ConsumeTicket(ctx context.Context, ticket string) (Crede
 
 		return Credential{}, err
 	}
-
-	repo.cache.Del(ctx, key)
 
 	var credential Credential
 	if err := json.Unmarshal(payload, &credential); err != nil {
